86eba38ce46cbf1d1cbdfddb68348bd5aafcb427
Nichole Mattera <me@nicholemattera.com>
2022-01-12 07:58:08 -0600

    📝 Updated copyright year

--- include/sini/option.hpp
+++ include/sini/option.hpp
@@ -1,6 +1,6 @@
 /*
  * Simple INI Parser
- * Copyright (c) 2021 Nichole Mattera
+ * Copyright (c) 2022 Nichole Mattera
  * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above 
